7ef16546474ef21be71d7080615805e7055893cc
giri-10 <90170719+giri-10@users.noreply.github.com>
2022-11-13 08:21:23 +0530

    login

--- jarvisVoice.py
+++ jarvisVoice.py
@@ -25,32 +25,39 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voices', voices[1].id)
 
-
-#login
-def Ok():
-    uname = e1.get()
+def Login_Info():
+    user_name = e1.get()
     password = e2.get()
  
-    if(uname == "" and password == "") :
-        messagebox.showinfo("", "Blank Not allowed")
+    if(user_name == "" and password == "") :
+        messagebox.showinfo("", "Please enter username and password")
  
  
-    elif(uname == "mvj1" and password == "1234"):
+    elif(user_name == "mvj1" and password == "1234"):
  
-        messagebox.showinfo("","Login Success")
+        messagebox.showinfo("","Login Successfull!")
         root.destroy()
  
     else :
-        messagebox.showinfo("","Incorrent Username and Password")
+        messagebox.showinfo("","Incorrect Username and Password")
  
  
 root = Tk()
-root.title("Login")
-root.geometry("300x200")
+root.title("Login: Jarvis")
+root.iconbitmap(r'C:\\Users\\Giridharan U\\Downloads\\j.ico') #change icon path
+
+
+# bgimg= tk.PhotoImage(file = r'C:\\Users\\Giridharan U\\Downloads\\download.ppm')
+# limg= Label(root, i=bgimg)
+# limg.pack()
+
+
+
+root.geometry("350x200")
 global e1
 global e2
  
-Label(root, text="UserName").place(x=10, y=10)
+Label(root, text="User name").place(x=10, y=10)
 Label(root, text="Password").place(x=10, y=40)
  
 e1 = Entry(root)
@@ -61,9 +68,10 @@ e2.place(x=140, y=40)
 e2.config(show="*")
  
  
-Button(root, text="Login", command=Ok ,height = 3, width = 13).place(x=10, y=100)
+Button(root, text="Login", command=Login_Info ,height = 2, width = 10).place(x=10, y=100)
  
 root.mainloop()
+
 
 
 #After login success, voice assistant starts
