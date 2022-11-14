0ae6c38e42afaeea4de74692feb88197c6960f70
abhimajeti24 <85148708+abhimajeti24@users.noreply.github.com>
2022-11-14 12:40:29 +0530

    Update jarvisVoice.py
    send help

--- jarvisVoice.py
+++ jarvisVoice.py
@@ -191,86 +191,6 @@ if __name__ == "__main__":
             time.sleep(10000)
             driver.quit()
 
-        elif 'todo list' and 'to do list' and 'Todo list'in query:
-                def newTask():
-                    task = my_entry.get()
-                    if task != "":
-                     lb.insert(END, task)
-                     my_entry.delete(0, "end")
-                    else:
-                     messagebox.showwarning("warning", "Please enter some task.")
-
-                def deleteTask():
-                    lb.delete(ANCHOR)
-    
-                ws = Tk()
-                ws.geometry('500x450+500+200')
-                ws.title('PythonGuides')
-                ws.config(bg='#223441')
-                ws.resizable(width=False, height=False)
-
-                frame = Frame(ws)
-                frame.pack(pady=10)
-
-                lb = Listbox(
-                    frame,
-                    width=25,
-                    height=8,
-                    font=('Times', 18),
-                    bd=0,
-                    fg='#464646',
-                    highlightthickness=0,
-                    selectbackground='#a6a6a6',
-                    activestyle="none",
-                    
-                )
-                lb.pack(side=LEFT, fill=BOTH)
-
-                task_list = []
-
-                for item in task_list:
-                    lb.insert(END, item)
-
-                sb = Scrollbar(frame)
-                sb.pack(side=RIGHT, fill=BOTH)
-
-                lb.config(yscrollcommand=sb.set)
-                sb.config(command=lb.yview)
-
-                my_entry = Entry(
-                    ws,
-                    font=('times', 24)
-                    )
-
-                my_entry.pack(pady=20)
-
-                button_frame = Frame(ws)
-                button_frame.pack(pady=20)
-
-                addTask_btn = Button(
-                    button_frame,
-                    text='Add Task',
-                    font=('times 14'),
-                    bg='#c5f776',
-                    padx=20,
-                    pady=10,
-                    command=newTask
-                )
-                addTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
-
-                delTask_btn = Button(
-                    button_frame,
-                    text='Delete Task',
-                    font=('times 14'),
-                    bg='#ff8b61',
-                    padx=20,
-                    pady=10,
-                    command=deleteTask
-                )
-                delTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
-
-
-                ws.mainloop()
 
         #category of jokes: neutral, twister, all
         elif 'tell jokes' in query:
@@ -278,7 +198,7 @@ if __name__ == "__main__":
             print(My_joke)
             speak(My_joke)
 
-        elif 'todays news' and 'news today' in query:
+        elif 'news' in query:
             news_url="https://news.google.com/news/rss"
             Client=urlopen(news_url)
             xml_page=Client.read()
@@ -296,4 +216,83 @@ if __name__ == "__main__":
                 print("-"*60)
 
 
+        elif 'Todo list' or 'to do list' in query:
+            def newTask():
+                task = my_entry.get()
+                if task != "":
+                 lb.insert(END, task)
+                 my_entry.delete(0, "end")
+                else:
+                 messagebox.showwarning("warning", "Please enter some task.")
+
+            def deleteTask():
+                lb.delete(ANCHOR)
+    
+            ws = Tk()
+            ws.geometry('500x450+500+200')
+            ws.title('PythonGuides')
+            ws.config(bg='#223441')
+            ws.resizable(width=False, height=False)
+
+            frame = Frame(ws)
+            frame.pack(pady=10)
+
+            lb = Listbox(
+                frame,
+                width=25,
+                height=8,
+                font=('Times', 18),
+                bd=0,
+                fg='#464646',
+                highlightthickness=0,
+                selectbackground='#a6a6a6',
+                activestyle="none",
+                )
+            lb.pack(side=LEFT, fill=BOTH)
+
+            task_list = []
+
+            for item in task_list:
+                lb.insert(END, item)
+
+            sb = Scrollbar(frame)
+            sb.pack(side=RIGHT, fill=BOTH)
+
+            lb.config(yscrollcommand=sb.set)
+            sb.config(command=lb.yview)
+
+            my_entry = Entry(
+                ws,
+                font=('times', 24)
+                )
+
+            my_entry.pack(pady=20)
+
+            button_frame = Frame(ws)
+            button_frame.pack(pady=20)
+
+            addTask_btn = Button(
+                button_frame,
+                text='Add Task',
+                font=('times 14'),
+                bg='#c5f776',
+                padx=20,
+                pady=10,
+                command=newTask
+            )
+            addTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
+
+            delTask_btn = Button(
+                button_frame,
+                text='Delete Task',
+                font=('times 14'),
+                bg='#ff8b61',
+                padx=20,
+                pady=10,
+                command=deleteTask
+            )
+            delTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
+
+
+            ws.mainloop()
         temp_variable += 1
