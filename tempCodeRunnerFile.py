a7757328dc3cc3e212b2c2715a9b383d5aa61960
Giridharan <90170719+giri-10@users.noreply.github.com>
2023-07-15 14:26:15 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-try:
-#               if 'google' in user_command.lower().strip():
-#                             break
-#               else:
-#                             print("No wake word found. Try again.")
-#             except Exception as e:
-#                     print("Error in  recognizing audio: ", e)
-#                     continue
